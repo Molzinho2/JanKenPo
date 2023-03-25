ea2bab3715db043849487a5a85118a703337f566
igor- <igor-@IGORMOL>
2023-03-25 20:46:28 -0300

    Adicionar arquivos de projeto.

---
+++ JanKenPo/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JanKenPo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartGame(0);
+        }
+
+        private void btnPapel_Click(object sender, EventArgs e)
+        {
+            StartGame(1);
+        }
+
+        private void btnTesoura_Click(object sender, EventArgs e)
+        {
+            StartGame(2);
+        }
+
+        private void StartGame(int opc)
+        {
+            labelResultado.Visible = false;
+            Game game = new Game();
+
+            switch (game.Jogar(opc))
+            {
+                case Game.Resultado.Ganhar:
+                    picResultado.BackgroundImage = Image.FromFile("images/Ganhar.png");
+                    goto default;
+                case Game.Resultado.Perder:
+                    picResultado.BackgroundImage = Image.FromFile("images/Perder.png");
+                    goto default;
+                case Game.Resultado.Empatar:
+                    picResultado.BackgroundImage = Image.FromFile("images/Empatar.png");
+                    goto default;
+                default:
+                    pictureBox1.Image = game.ImgJogador;
+                    pictureBox2.Image = game.ImgPC;
+                    break;
+                
+            }
+        }
+    }
+
+}
